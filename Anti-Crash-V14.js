9dcf77c63975b3f401ac18a2cf0c9a0b88051928
Bastian <104389376+Bastian-Community@users.noreply.github.com>
2022-08-30 00:05:23 +0200

    added v14 & changed names

---
+++ Anti-Crash-V14.js
@@ -0,0 +1,103 @@
+const { EmbedBuilder, WebhookClient } = require("discord.js");
+const { inspect } = require("util");
+const webhook = new WebhookClient({
+  url: "https://discord.com/api/webhooks/1011789787042152478/lSwS45ocOFeRKjK02YMbXSIGwXH5TT6vR1ojg4aonSX0QsmF4awRqO4HUBKvih31R9d5",
+});
+
+module.exports = (client) => {
+  const embed = new EmbedBuilder().setColor("Red");
+
+  client.on("error", (err) => {
+    console.log(err);
+
+    embed
+      .setTitle("Discord API Error")
+      .setURL("https://discordjs.guide/popular-topics/errors.html#api-errors")
+      .setDescription(
+        `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``
+      )
+      .setTimestamp();
+
+    return webhook.send({ embeds: [embed] });
+  });
+
+  process.on("unhandledRejection", (reason, promise) => {
+    console.log(reason, "\n", promise);
+
+    embed
+      .setTitle("Unhandled Rejection/Catch")
+      .setURL("https://nodejs.org/api/process.html#event-unhandledrejection")
+      .addFields(
+        {
+          name: "Reason",
+          value: `\`\`\`${inspect(reason, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        },
+        {
+          name: "Promise",
+          value: `\`\`\`${inspect(promise, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        }
+      )
+      .setTimestamp();
+
+    return webhook.send({ embeds: [embed] });
+  });
+
+  process.on("uncaughtException", (err, origin) => {
+    console.log(err, "\n", origin);
+
+    embed
+      .setTitle("Uncaught Exception/Catch")
+      .setURL("https://nodejs.org/api/process.html#event-uncaughtexception")
+      .addFields(
+        {
+          name: "Error",
+          value: `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        },
+        {
+          name: "Origin",
+          value: `\`\`\`${inspect(origin, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        }
+      )
+      .setTimestamp();
+
+    return webhook.send({ embeds: [embed] });
+  });
+
+  process.on("uncaughtExceptionMonitor", (err, origin) => {
+    console.log(err, "\n", origin);
+
+    embed
+      .setTitle("Uncaught Exception Monitor")
+      .setURL(
+        "https://nodejs.org/api/process.html#event-uncaughtexceptionmonitor"
+      )
+      .addFields(
+        {
+          name: "Error",
+          value: `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        },
+        {
+          name: "Origin",
+          value: `\`\`\`${inspect(origin, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        }
+      )
+      .setTimestamp();
+
+    return webhook.send({ embeds: [embed] });
+  });
+
+  process.on("warning", (warn) => {
+    console.log(warn);
+
+    embed
+      .setTitle("Uncaught Exception Monitor Warning")
+      .setURL("https://nodejs.org/api/process.html#event-warning")
+      .addFields({
+        name: "Warning",
+        value: `\`\`\`${inspect(warn, { depth: 0 }).slice(0, 1000)}\`\`\``,
+      })
+      .setTimestamp();
+
+    return webhook.send({ embeds: [embed] });
+  });
+};
